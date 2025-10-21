420f5016730569658e71eaa9057e5e57daedbbed
Daniel Stepp <daniel.stepp@popmenu.com>
2025-10-21 19:32:20 -0400

    Integrated Unistyles

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['react-native-unistyles/plugin', {
+        // Pass root folder of your application
+        // All files under this folder will be processed by the Babel plugin
+        root: './app',
+      }]
+    ]
+  };
+}
