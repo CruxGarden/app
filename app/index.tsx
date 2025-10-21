bff6bfe2030685ed83323f852d36a1e3f3bb3446
Will Stepp <willstepp@gmail.com>
2025-10-21 19:32:20 -0400

    Integrated Unistyles

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
 import { Text, View } from "react-native";
+import { StyleSheet } from 'react-native-unistyles';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Hello world from Unistyles</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+   container: {
+     backgroundColor: 'red'
+   }
+});
